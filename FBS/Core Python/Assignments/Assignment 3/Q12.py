3323d4168ff9bacafc508102da905ed7ebe4108b
Rahul Mete <rahulmete9096@gmail.com>
2026-08-31 18:41:39 +0530

    Enhance palindrome check for 3-digit numbers
    Added an alternative method to check for palindrome.

--- FBS/Core Python/Assignments/Assignment 3/Q12.py
+++ FBS/Core Python/Assignments/Assignment 3/Q12.py
@@ -9,3 +9,17 @@ if num >= 100 and num <= 999:
         print("Number is not palidroome")
 else:
     print("Enter a 3 digit number")
+
+#OR
+
+num = int(input("Enter number: "))
+temp = num 
+rev_num = 0
+while(temp>0):
+    d = temp % 10
+    temp = temp // 10
+    rev_num = rev_num * 10 + d
+if(rev_num == num):
+    print("Number is palidrome")
+else:
+    print("Number is not palidroome")
